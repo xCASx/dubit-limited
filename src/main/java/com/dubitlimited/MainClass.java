737c85375333a970ba2db4063059c1d4e23a32fc
Maxim Kolesnikov <v-mkolesnikov@expedia.com>
2016-02-02 19:27:13 -0600

    Add null check to Localizer constructor and public method

--- src/main/java/com/dubitlimited/MainClass.java
+++ src/main/java/com/dubitlimited/MainClass.java
@@ -26,8 +26,18 @@ class Localizer {
 
     private final String resourceBundleName;
     private HashMap<Locale, ResourceBundle> resourceBundles = new HashMap<>();
+    private ResourceBundle defaultResourceBundle;
 
+    /**
+     * Constructor for Localizer class
+     * @param resourceBundleName - name of resource bundle
+     * @throws IllegalArgumentException if resource bundle name is {@code null}
+     */
     public Localizer(String resourceBundleName) {
+        if (resourceBundleName == null) {
+            throw new IllegalArgumentException("Resource bundle name cannot be null");
+        }
+
         this.resourceBundleName = resourceBundleName;
     }
 
@@ -39,10 +49,22 @@ class Localizer {
      * @return localized formatted string
      */
     public String getMessage(Locale locale, String msgCode, Object... parameters) {
-        ResourceBundle resourceBundle = resourceBundles.get(locale);
-        if (resourceBundle == null) {
-            resourceBundle = createNewBundle(locale);
-            resourceBundles.put(locale, resourceBundle);
+        if (msgCode == null) {
+            throw new IllegalArgumentException("Message code cannot be null");
+        }
+
+        ResourceBundle resourceBundle;
+        if (locale == null) {
+            if (defaultResourceBundle == null) {
+                defaultResourceBundle = createDefaultBundle();
+            }
+            resourceBundle = defaultResourceBundle;
+        } else {
+            resourceBundle = resourceBundles.get(locale);
+            if (resourceBundle == null) {
+                resourceBundle = createNewBundle(locale);
+                resourceBundles.put(locale, resourceBundle);
+            }
         }
         return MessageFormat.format(resourceBundle.getString(msgCode), parameters);
     }
@@ -54,5 +76,13 @@ class Localizer {
      */
     private ResourceBundle createNewBundle(Locale locale) {
         return ResourceBundle.getBundle(resourceBundleName, locale);
+    }
+
+    /**
+     * Create default resource bundle
+     * @return new resource bundle
+     */
+    private ResourceBundle createDefaultBundle() {
+        return ResourceBundle.getBundle(resourceBundleName);
     }
 }
