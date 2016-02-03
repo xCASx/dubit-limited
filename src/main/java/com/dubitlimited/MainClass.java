ad7b922b91425f0c62d01566172a45119ab26ec9
Maxim Kolesnikov <v-mkolesnikov@expedia.com>
2016-02-02 18:33:37 -0600

    Add message bundle support to Localiser class

--- src/main/java/com/dubitlimited/MainClass.java
+++ src/main/java/com/dubitlimited/MainClass.java
@@ -1,6 +1,8 @@
 package com.dubitlimited;
 
 import java.util.HashMap;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class MainClass {
 
@@ -17,8 +19,15 @@ class Localiser {
 
     private HashMap<String, HashMap<String, String>> translations;
 
+    private HashMap<Locale, ResourceBundle> resourceBundles = new HashMap<>();
+    private String resourceBundleName;
+
     public Localiser() {
         this.translations = new HashMap<String, HashMap<String, String>>();
+    }
+
+    public Localiser(String resourceBundleName) {
+        this.resourceBundleName = resourceBundleName;
     }
 
     public void add(String locale, String key, String value) {
@@ -31,5 +40,31 @@ class Localiser {
 
     public String find(String locale, String key, String parameter) {
         return translations.get(locale).get(key).replace("#{0}", parameter);
+    }
+
+    /**
+     * Get localized formatted message from resource bundle.
+     * @param locale - current locale
+     * @param msgCode - message code
+     * @param parameter - dynamic part of message
+     * @return localized formatted string
+     */
+    public String getMessage(Locale locale, String msgCode, String parameter) {
+        ResourceBundle resourceBundle = resourceBundles.get(locale);
+        if (resourceBundle == null) {
+            resourceBundle = createNewBundle(locale);
+            resourceBundles.put(locale, resourceBundle);
+        }
+
+        return resourceBundle.getString(msgCode).replace("#{0}", parameter);
+    }
+
+    /**
+     * Create new resource bundle for given locale
+     * @param locale - current locale
+     * @return new resource bundle
+     */
+    private ResourceBundle createNewBundle(Locale locale) {
+        return ResourceBundle.getBundle(resourceBundleName, locale);
     }
 }
